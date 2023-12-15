8d77faae56c791cac9d160192580c1d363ebf8f6
Matt Lindeman <112662135+MattLindeman@users.noreply.github.com>
2023-12-14 19:38:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import seaborn as sns
+import matplotlib
+matplotlib.use('Agg')  # Set the backend to 'Agg'
 import matplotlib.pyplot as plt
 import streamlit as st
 from io import BytesIO
